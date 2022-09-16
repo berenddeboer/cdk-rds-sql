767b923e9a92d7edee4facf54a64c0c751820e49
Berend de Boer <berend@pobox.com>
2022-09-16 15:59:51 +1200

    chore: projen after first build

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,18 @@
-import { typescript } from 'projen';
-const project = new typescript.TypeScriptProject({
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  projenrcTs: true,
+  author: 'Berend de Boer',
+  authorAddress: 'berend@pobox.com',
+  authorEmail: 'berend@pobox.com',
+  cdkVersion: '2.41.0',
+  constructsVersion: '10.1.101',
   defaultReleaseBranch: 'main',
   name: 'ts-test',
-  projenrcTs: true,
-
-  // deps: [],                /* Runtime dependencies of this module. */
+  repositoryUrl: 'https://github.com/berenddeboer/cdk-rds-sql.git',
+  deps: [],
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
+  devDeps: [],
   // packageName: undefined,  /* The "name" in package.json. */
 });
+project.addGitIgnore('*~');
 project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="ts-test.Hello"></a>
+
+#### Initializers <a name="Initializers" id="ts-test.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from 'ts-test'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#ts-test.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="ts-test.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
