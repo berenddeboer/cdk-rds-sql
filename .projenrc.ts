ff0594b73374cf2fdc4b016da167c63473c716a4
Berend de Boer <berend@pobox.com>
2022-09-16 15:59:51 +1200

    chore: add pre-commit support

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,18 +1,28 @@
-import { awscdk } from 'projen';
+import { awscdk } from "projen"
 const project = new awscdk.AwsCdkConstructLibrary({
   projenrcTs: true,
-  author: 'Berend de Boer',
-  authorAddress: 'berend@pobox.com',
-  authorEmail: 'berend@pobox.com',
-  cdkVersion: '2.41.0',
-  constructsVersion: '10.1.101',
-  defaultReleaseBranch: 'main',
-  name: 'ts-test',
-  repositoryUrl: 'https://github.com/berenddeboer/cdk-rds-sql.git',
+  author: "Berend de Boer",
+  authorAddress: "berend@pobox.com",
+  authorEmail: "berend@pobox.com",
+  cdkVersion: "2.41.0",
+  constructsVersion: "10.1.101",
+  defaultReleaseBranch: "main",
+  name: "ts-test",
+  repositoryUrl: "https://github.com/berenddeboer/cdk-rds-sql.git",
+  eslint: true,
+  jestOptions: {
+    jestVersion: "28",
+  },
   deps: [],
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   devDeps: [],
   // packageName: undefined,  /* The "name" in package.json. */
-});
-project.addGitIgnore('*~');
-project.synth();
+})
+project.addGitIgnore("*~")
+if (project.eslint) {
+  project.eslint.addRules({
+    semi: ["off"],
+    quotes: ["error", "double"],
+  })
+}
+project.synth()
