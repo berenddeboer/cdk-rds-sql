4ce8ed94b4846ebd329b32569053f957aa2b7bab
Berend de Boer <berend@pobox.com>
2024-05-11 20:02:43 +1200

    vendor: upgrade to projen 0.81.10
    Seems to be a massive upgrade, so not sure everything still works.

--- .projenrc.ts
+++ .projenrc.ts
@@ -62,14 +62,14 @@ if (project.eslint) {
     quotes: ["error", "double"],
   })
 }
-const buildWorkflow = project.tryFindObjectFile(".github/workflows/build.yml")
-if (buildWorkflow && buildWorkflow.patch) {
-  buildWorkflow.patch(JsonPatch.add("/jobs/build/container/options", "--group-add sudo"))
-}
-const releaseWorkflow = project.tryFindObjectFile(".github/workflows/release.yml")
-if (releaseWorkflow && releaseWorkflow.patch) {
-  releaseWorkflow.patch(
-    JsonPatch.add("/jobs/release/container/options", "--group-add sudo")
-  )
-}
+// const buildWorkflow = project.tryFindObjectFile(".github/workflows/build.yml")
+// if (buildWorkflow && buildWorkflow.patch) {
+//   buildWorkflow.patch(JsonPatch.add("/jobs/build/container/options", "--group-add sudo"))
+// }
+// const releaseWorkflow = project.tryFindObjectFile(".github/workflows/release.yml")
+// if (releaseWorkflow && releaseWorkflow.patch) {
+//   releaseWorkflow.patch(
+//     JsonPatch.add("/jobs/release/container/options", "--group-add sudo")
+//   )
+// }
 project.synth()
